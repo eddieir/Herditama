a91704c9fbcdafd619cd2e48279d9eb3c64ce607
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-04-01 20:53:16 +0200

    udpate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import Services from '../components/Services';
+
 
 export const Home = () => {
   return (
-    <div>
-      Hello from home page
-    </div>
+    <>
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="delux rooms starting from $299">
+          <Link to='/rooms' className='btn-primary'>
+            Our rooms
+        </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
+
   )
 }
 
